aca92282205f03f92ed9660c31d3f17526265881
Joshua Davidson <joshua.davidsonsga@gmail.com>
2020-12-17 12:18:52 -0600

    finished OO easy exercises

--- exercises/small_problems/test.rb
+++ exercises/small_problems/test.rb
@@ -1,12 +1,31 @@
-class Person
-  attr_writer :first_name, :last_name
+def reverse_num(arry)
+    reverse_arry = []
+    num = 0
+    arry.each do |elem|
+      reverse_arry[num]=[]
+        elem.each_char do |str|
+          reverse_arry[num] << str if "123456789".include?(str)
+        end
+        num += 1
+    end
+ 
+  reverse_arry.map! {|x| x.reverse}
+  p reverse_arry
+  count = 0
 
-  def full_name
-    first_name + ' ' + last_name
+  answer = arry.map do |elem|
+      new_word = elem.chars.map do |str|
+          if "123456789".include?(str)
+              reverse_arry[count].shift
+          else
+              str
+          end
+      end
+      count += 1
+      new_word.join("")
   end
+  answer
 end
 
-mike = Person.new
-mike.first_name = 'Michael'
-mike.last_name = 'Garcia'
-p mike.full_name # => 'Michael Garcia'
+p reverse_num(["abd89c"])
+p reverse_num(["RBi24klm1895","abd123def456"])
